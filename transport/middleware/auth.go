f33b203b4dba2e1780453c812e55b24392761f78
mucha-fauzy <muchamad.fauzy@evermos.com>
2023-08-04 17:03:32 +0700

    add chaining

--- transport/middleware/auth.go
+++ transport/middleware/auth.go
@@ -20,63 +20,61 @@ func ProvideAuthentication(db *infras.Conn, secret []byte) *Authentication {
 	}
 }
 
-func (a *Authentication) VerifyTeacherJWT(next http.Handler) http.Handler {
+func IsTeacherRole(tokenString string) (bool, error) {
+	// Make an HTTP GET request to the bootcamp-auth microservice to validate the JWT
+	authServiceURL := "http://localhost:8080/v1/validate-auth"
+	req, err := http.NewRequest("GET", authServiceURL, nil)
+	if err != nil {
+		return false, err
+	}
+
+	req.Header.Set("Authorization", "Bearer "+tokenString)
+
+	client := &http.Client{}
+	resp, err := client.Do(req)
+	if err != nil {
+		return false, err
+	}
+	defer resp.Body.Close()
+
+	if resp.StatusCode == http.StatusOK {
+		var responseData map[string]string
+		err := json.NewDecoder(resp.Body).Decode(&responseData)
+		if err != nil {
+			return false, err
+		}
+
+		role, ok := responseData["role"]
+		if !ok {
+			return false, nil
+		}
+
+		return role == "teacher", nil
+	}
+
+	return false, nil
+}
+
+func (a *Authentication) VerifyJWT(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		// Get the JWT token from the Authorization header
 		authHeader := r.Header.Get("Authorization")
 		tokenString := strings.Replace(authHeader, "Bearer ", "", 1)
 
-		// Make an HTTP GET request to the bootcamp-auth microservice to validate the JWT
-		authServiceURL := "http://localhost:8080/v1/validate-auth"
-		req, err := http.NewRequest("GET", authServiceURL, nil)
+		isTeacher, err := IsTeacherRole(tokenString)
 		if err != nil {
-			http.Error(w, "Error creating request", http.StatusInternalServerError)
+			http.Error(w, "Error validating role", http.StatusInternalServerError)
 			return
 		}
 
-		req.Header.Set("Authorization", "Bearer "+tokenString)
-
-		client := &http.Client{}
-		resp, err := client.Do(req)
-		if err != nil {
-			http.Error(w, "Error sending request", http.StatusInternalServerError)
-			return
-		}
-		defer resp.Body.Close()
-
-		// Check the response status code to see if the token is valid
-		if resp.StatusCode == http.StatusOK {
-			// Token is valid, extract the user's role from the response body
-			var responseData map[string]string
-			err := json.NewDecoder(resp.Body).Decode(&responseData)
-			if err != nil {
-				http.Error(w, "Failed to parse response", http.StatusInternalServerError)
-				return
-			}
-
-			role, ok := responseData["role"]
-
-			if !ok {
-				http.Error(w, "Failed to get user role", http.StatusInternalServerError)
-				return
-			}
-
-			if role != "teacher" {
-				http.Error(w, "Unauthorized", http.StatusUnauthorized)
-				return
-			}
-
-			// User is authorized, add the token to the request context for use in protected endpoints
-			ctx := context.WithValue(r.Context(), "user", tokenString)
-			r = r.WithContext(ctx)
-			next.ServeHTTP(w, r)
-		} else if resp.StatusCode == http.StatusUnauthorized {
-			// Token is invalid or expired
-
+		if !isTeacher {
 			http.Error(w, "Unauthorized", http.StatusUnauthorized)
-		} else {
-
-			http.Error(w, "Error", http.StatusInternalServerError)
+			return
 		}
+
+		// User is authorized, add the token to the request context for use in protected endpoints
+		ctx := context.WithValue(r.Context(), "user", tokenString)
+		r = r.WithContext(ctx)
+		next.ServeHTTP(w, r)
 	})
 }
